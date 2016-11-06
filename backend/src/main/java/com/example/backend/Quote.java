328f82927926ad39df7b543923721943429555ce
Ajk2rxKamehameha <ajeet.meena@gmail.com>
2016-11-06 13:46:07 +0530

    Added Text (which can store more then 500 words of data).

--- backend/src/main/java/com/example/backend/Quote.java
+++ backend/src/main/java/com/example/backend/Quote.java
@@ -1,38 +1,49 @@
 package com.example.backend;
 
+import com.google.appengine.api.datastore.Text;
 import com.googlecode.objectify.annotation.Entity;
 import com.googlecode.objectify.annotation.Id;
 
 @Entity
 public class Quote {
- @Id
- private Long id;
- private String who;
- private String what;
+    @Id
+    private Long id;
+    private String who;
+    private String what;
+    private Text text;
 
-public Quote() {}
+    public Quote() {
+    }
 
-public Long getId() {
- return id;
- }
+    public Long getId() {
+        return id;
+    }
 
-public void setId(Long id) {
- this.id = id;
- }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-public String getWho() {
- return who;
- }
+    public String getWho() {
+        return who;
+    }
 
-public void setWho(String who) {
- this.who = who;
- }
+    public void setWho(String who) {
+        this.who = who;
+    }
 
-public String getWhat() {
- return what;
- }
+    public String getWhat() {
+        return what;
+    }
 
-public void setWhat(String what) {
- this.what = what;
- }
+    public void setWhat(String what) {
+        this.what = what;
+    }
+
+    public Text getText() {
+        return text;
+    }
+
+    public void setText(Text text) {
+        this.text = text;
+    }
 }
