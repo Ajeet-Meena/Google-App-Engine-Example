1e17dd3b90a102bdf3cd9f6f76745659ad165084
Ajk2rxKamehameha <ajeet.meena@gmail.com>
2016-11-08 08:45:51 +0530

    Added Getter and Setter for Author.

--- backend/src/main/java/com/example/backend/Auther.java
+++ backend/src/main/java/com/example/backend/Auther.java
@@ -13,4 +13,19 @@ public class Auther {
     private Long id;
     String name;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
