9063295938a286a1bd6e230653d7da9821aa383e
Ajk2rxKamehameha <ajeet.meena@gmail.com>
2016-12-11 11:26:51 +0530

    Renamed Android app package name.

--- app/src/androidTest/java/com/ajeetmeena/googleappengine/ApplicationTest.java
+++ app/src/androidTest/java/com/ajeetmeena/googleappengine/ApplicationTest.java
@@ -1,4 +1,4 @@
-package com.ajeetmeena.googleappengine;
+package io.wallmag.android;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
--- app/src/main/java/io/wallmag/android/MainActivity.java
+++ app/src/main/java/io/wallmag/android/MainActivity.java
@@ -1,4 +1,4 @@
-package com.ajeetmeena.googleappengine;
+package io.wallmag.android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
